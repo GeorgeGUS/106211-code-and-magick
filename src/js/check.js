3ed662d3c3dd2c68265425e991b60b86e47115ca
GeorgeGUS <georgegus1@gmail.com>
2016-10-07 00:56:26 +0300

    Вынес все переменные в начало функции, заменил лишние return одним в конце, убрал лишнюю проверку на массив

--- src/js/check.js
+++ src/js/check.js
@@ -1,27 +1,30 @@
 "use strict";
 
 function getMessage(a, b) {
+  var i = 0,
+    result = 0,
+    distancePath = 0,
+    numberOfSteps = 0;
   if (typeof a === "boolean") {
-    return a ? "Я попал в " + b : "Я никуда не попал";
+    result = a ? "Я попал в " + b : "Я никуда не попал";
     
   } else if (typeof a === "number") {
-    return "Я прыгнул на " + a * 100 + " сантиметров";
+    result = "Я прыгнул на " + a * 100 + " сантиметров";
     
-  } else if (Array.isArray(a) && Array.isArray(b)) {
-    var distancePath = 0;
-    for (var i = 0; i < a.length; i++) {
+  } else if (Array.isArray(b)) {
+    for (i; i < a.length; i++) {
       distancePath += a[i] * b[i];
     }
-    return "Я прошёл " + distancePath + " метров";
+    result = "Я прошёл " + distancePath + " метров";
     
   } else if (Array.isArray(a)) {
-    var numberOfSteps = 0;
-    for (var i = 0; i < a.length; i++) {
+    for (i; i < a.length; i++) {
       numberOfSteps += a[i];
     }
-    return "Я прошёл " + numberOfSteps + " шагов";
+    result = "Я прошёл " + numberOfSteps + " шагов";
     
   } else {
-    return "Переданы некорректные данные";
+    result = "Переданы некорректные данные";
   }
+  return result;
 }
