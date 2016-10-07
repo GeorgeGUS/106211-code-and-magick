9e253bee6df9b3d61428a7f49ed9bda6dd07dced
GeorgeGUS <georgegus1@gmail.com>
2016-10-07 12:23:42 +0300

    Удалил лишнюю проверку атрибута a на массив

--- src/js/check.js
+++ src/js/check.js
@@ -1,30 +1,28 @@
-"use strict";
-
 function getMessage(a, b) {
+  "use strict";
   var i = 0,
     result = 0,
     distancePath = 0,
     numberOfSteps = 0;
+
   if (typeof a === "boolean") {
     result = a ? "Я попал в " + b : "Я никуда не попал";
-    
   } else if (typeof a === "number") {
     result = "Я прыгнул на " + a * 100 + " сантиметров";
-    
-  } else if (Array.isArray(b)) {
-    for (i; i < a.length; i++) {
-      distancePath += a[i] * b[i];
-    }
-    result = "Я прошёл " + distancePath + " метров";
-    
   } else if (Array.isArray(a)) {
-    for (i; i < a.length; i++) {
-      numberOfSteps += a[i];
+    if (Array.isArray(b)) {
+      for (i; i < a.length; i++) {
+        distancePath += a[i] * b[i];
+      }
+      result = "Я прошёл " + distancePath + " метров";
+    } else {
+      for (i; i < a.length; i++) {
+        numberOfSteps += a[i];
+      }
+      result = "Я прошёл " + numberOfSteps + " шагов";
     }
-    result = "Я прошёл " + numberOfSteps + " шагов";
-    
   } else {
     result = "Переданы некорректные данные";
   }
   return result;
-}
+} 
