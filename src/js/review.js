266dda25c883d77fe44301a8d8e56a779cc6f3c0
GeorgeGUS <georgegus1@gmail.com>
2016-10-24 23:29:50 +0300

    Создал новые модули загрузки и отрисовки шаблона

---
+++ src/js/review.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+/**
+ * Отрисивка одного блока отзыва
+ * @param {Array} reviews
+ * @returns {Node} reviewItem
+ */
+module.exports = function (reviews) {
+  var template = document.getElementById('review-template'),
+    templateContainer = 'content' in template ? template.content : template,
+    reviewItem = templateContainer.cloneNode(true),
+    reviewContainer = reviewItem.querySelector('.review'),
+    reviewPicture = reviewItem.querySelector('.review-author'),
+    reviewText = reviewItem.querySelector('.review-text'),
+    ratingClasses = ['one', 'two', 'three', 'four', 'five'],
+    authorImage = new Image(124, 124);
+
+  authorImage.onload = function () {
+    reviewPicture.src = this.src;
+  };
+  authorImage.onerror = function () {
+    reviewContainer.classList.add('review-load-failure');
+  };
+  authorImage.src = reviews.author.picture;
+  reviewText.textContent = reviews.description;
+  reviewItem.querySelector('.review-rating').classList.add('review-rating-' + ratingClasses[reviews.rating - 1]);
+
+  return reviewItem;
+};
+
