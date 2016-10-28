92f95d547210e66645ddceae9c765a4b3308556f
GeorgeGUS <georgegus1@gmail.com>
2016-10-28 18:55:49 +0300

    Подключил модуль gallery.js, загрузил массив адресов фотографий и подключил к ним обработчик по клику, вызывающий появление слайдера

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var Game = require('./game');
+var Gallery = require('./gallery');
 var form = require('./form');
 var reviews = require('./reviews');
 
 var game = new Game(document.querySelector('.demo'));
 game.initializeLevelAndStart();
 game.setGameStatus(Game.Verdict.INTRO);
+
+var pictures = document.querySelectorAll('.photogallery-image');
+
+var gallery = new Gallery();
+
+pictures.forEach(function(picture, pictureNum) {
+  picture.onclick = function() {
+    gallery.show(pictureNum);
+  };
+});
 
 var formOpenButton = document.querySelector('.reviews-controls-new');
 
