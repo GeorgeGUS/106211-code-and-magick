27836899facfb479ef350d4585faf646f1c9ef81
GeorgeGUS <georgegus1@gmail.com>
2016-10-31 17:05:28 +0300

    Изменил способ подключения обработчиков клика к элементам ответов и проверки их на активный элемент, убрал лишний return в конце конструктора, вынес метод remove в объект прототипа конструктора

--- src/js/review-constructor.js
+++ src/js/review-constructor.js
@@ -2,7 +2,7 @@
 
 var Review = function(element, data) {
   this.data = data;
-  this.element = element(data);
+  this.element = element;
 
   var self = this;
 
@@ -12,35 +12,23 @@ var Review = function(element, data) {
    */
   var CLASS_ACTIVE = 'review-quiz-answer-active';
 
-  this.answerPositive = this.element.querySelector('.review-quiz-answer-yes');
-  this.answerNegative = this.element.querySelector('.review-quiz-answer-no');
+  this.answers = this.element.querySelectorAll('.review-quiz-answer');
 
-  /**
-   * Подключение обработчика события по клику на один из ответов
-   * и проверка их на активное состояние
-   * @param {Node} pros выбранный ответ
-   * @param {Node} cons противоположный ответ
-   */
-  var setAnswerListener = function(pros, cons) {
-    pros.onclick = function() {
-      if (cons.classList.contains(CLASS_ACTIVE)) {
-        cons.classList.remove(CLASS_ACTIVE);
-        pros.classList.add(CLASS_ACTIVE);
-      } else {
-        pros.classList.add(CLASS_ACTIVE);
+  for (var i = 0; i < this.answers.length; i++) {
+    this.answers[i].onclick = function() {
+      for (i = 0; i < self.answers.length; i++) {
+        self.answers[i].classList.remove(CLASS_ACTIVE);
       }
+      this.classList.add(CLASS_ACTIVE);
     };
-  };
+  }
+};
 
-  setAnswerListener(self.answerPositive, self.answerNegative);
-  setAnswerListener(self.answerNegative, self.answerPositive);
-
-  this.remove = function() {
-    this.answerPositive.onclick = null;
-    this.answerNegative.onclick = null;
-  };
-
-  return this.element;
+Review.prototype = {
+  remove: function() {
+    this.answerYes.onclick = null;
+    this.answerNo.onclick = null;
+  }
 };
 
 module.exports = Review;
