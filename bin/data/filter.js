dce29ed1c039f89d70be0e1f35f78c09d8a2b300
GeorgeGUS <georgegus1@gmail.com>
2016-11-05 12:25:19 +0300

    Добавил аннотации и сделал один возврат списка в конце модуля

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,37 +1,45 @@
 'use strict';
 
+/**
+ * Задание порядка и фильрации отзывов
+ * в зависимости от выбранного фильтра
+ * @param {Array} list
+ * @param {string} filterID
+ */
 module.exports = function(list, filterID) {
+  var newList = [];
   var lastThreeDays = Date.now() - 1000 * 3600 * 24 * 3;
-
   switch (filterID) {
     case 'reviews-all':
-      return list;
+      newList = list;
      break;
     case 'reviews-recent':
-      return list.filter(function (listItem) {
+      newList = list.filter(function (listItem) {
         return listItem.created <= lastThreeDays;
       }).sort(function (a, b) {
         return b.created - a.created;
       });
       break;
     case 'reviews-good':
-      return list.filter(function(listItem) {
+      newList = list.filter(function(listItem) {
         return listItem.rating >= 3;
       }).sort(function(a, b) {
         return b.rating - a.rating;
       });
       break;
     case 'reviews-bad':
-      return list.filter(function(listItem) {
+      newList = list.filter(function(listItem) {
         return listItem.rating < 3;
       }).sort(function(a, b) {
         return a.rating - b.rating;
       });
       break;
     case 'reviews-popular':
-      return list.slice().sort(function(a, b) { //slice() здесь служит для создания копии исходного массива
+      newList = list.slice().sort(function(a, b) { //slice() здесь служит для создания копии исходного массива
         return b.review_usefulness - a.review_usefulness;
       });
       break;
   }
+
+  return newList;
 };
