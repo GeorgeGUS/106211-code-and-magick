46500e27732b0e98701ab5dc467da452d976fbb1
GeorgeGUS <georgegus1@gmail.com>
2016-11-10 01:31:15 +0300

    В модуле review перенёс объявление методов и обработчиков в прототип, привязал контекст методом bind

--- src/js/review.js
+++ src/js/review.js
@@ -1,63 +1,69 @@
 'use strict';
 
+/**
+ * @const
+ * @type {string}
+ */
+var CLASS_ACTIVE = 'review-quiz-answer-active';
 
 /**
- * Отрисивка одного блока отзыва
- * @param {Array} reviews
- * @returns {Node} reviewItem
+ * Конструктор блока отзыва
+ * @param {Array} data
+ * @constructor
  */
-var getReviewItem = function(reviews) {
-  var template = document.getElementById('review-template'),
-    templateContainer = 'content' in template ? template.content : template,
-    reviewItem = templateContainer.cloneNode(true),
-    reviewContainer = reviewItem.querySelector('.review'),
-    reviewPicture = reviewItem.querySelector('.review-author'),
-    reviewText = reviewItem.querySelector('.review-text'),
-    ratingClasses = ['one', 'two', 'three', 'four', 'five'],
-    authorImage = new Image(124, 124);
-
-  authorImage.onload = function() {
-    reviewPicture.src = this.src;
-  };
-  authorImage.onerror = function() {
-    reviewContainer.classList.add('review-load-failure');
-  };
-  authorImage.src = reviews.author.picture;
-  reviewText.textContent = reviews.description;
-  reviewItem.querySelector('.review-rating').classList.add('review-rating-' + ratingClasses[reviews.rating - 1]);
-
-  return reviewItem;
-};
-
 var Review = function(data) {
   this.data = data;
-  this.element = getReviewItem(this.data);
-
-  var self = this;
-
-  /**
-   * @const
-   * @type {string}
-   */
-  var CLASS_ACTIVE = 'review-quiz-answer-active';
-
+  this.element = this.getReviewItem();
+  this.onAnswerClick = this.onAnswerClick.bind(this);
   this.answers = this.element.querySelectorAll('.review-quiz-answer');
 
-  Array.prototype.forEach.call(this.answers, function(answer) {
-    answer.addEventListener('click', function(evt) {
-      for (var i = 0; i < self.answers.length; i++) {
-        self.answers[i].classList.remove(CLASS_ACTIVE);
-      }
-      evt.target.classList.add(CLASS_ACTIVE);
-    });
-  });
+  for (var i = 0; i < this.answers.length; i++) {
+    this.answers[i].addEventListener('click', this.onAnswerClick);
+  }
 };
 
 Review.prototype = {
+  /**
+   * Отрисивка одного блока отзыва
+   * @returns {Node} reviewItem
+   */
+  getReviewItem: function() {
+    var template = document.getElementById('review-template'),
+      templateContainer = 'content' in template ? template.content : template,
+      reviewItem = templateContainer.cloneNode(true),
+      reviewContainer = reviewItem.querySelector('.review'),
+      reviewPicture = reviewItem.querySelector('.review-author'),
+      reviewText = reviewItem.querySelector('.review-text'),
+      ratingClasses = ['one', 'two', 'three', 'four', 'five'],
+      authorImage = new Image(124, 124);
+
+    authorImage.onload = function() {
+      reviewPicture.src = this.src;
+    };
+    authorImage.onerror = function() {
+      reviewContainer.classList.add('review-load-failure');
+    };
+    authorImage.src = this.data.author.picture;
+    reviewText.textContent = this.data.description;
+    reviewItem.querySelector('.review-rating').classList.add('review-rating-' + ratingClasses[this.data.rating - 1]);
+
+    return reviewItem;
+  },
+
+  /**
+   * @param {Node} evt
+   */
+  onAnswerClick: function(evt) {
+    for (var i = 0; i < this.answers.length; i++) {
+      this.answers[i].classList.remove(CLASS_ACTIVE);
+    }
+    evt.target.classList.add(CLASS_ACTIVE);
+  },
+
   remove: function() {
-    Array.prototype.forEach.call(this.answers, function(answer) {
-      answer.removeEventListener('click');
-    });
+    for (var i = 0; i < this.answers.length; i++) {
+      this.answers[i].removeEventListener('click', this.onAnswerClick);
+    }
   }
 };
 
