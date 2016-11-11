a98647b27f0ab3d689231bec8975eac4f9468f04
GeorgeGUS <georgegus1@gmail.com>
2016-11-11 14:59:16 +0300

    1ч. Создал в модуле util фукцию для продления цепочки прототипов через пустой конструктор, переписал модуль util для экспорта нескольких функций и переписал в game.js require нужной функции

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var throttle = require('./util');
+var throttle = require('./util').throttle;
 
 /**
  * @const
--- src/js/util.js
+++ src/js/util.js
@@ -1,22 +1,38 @@
 'use strict';
 
-/**
- * Универсальная функция для оптимизации функций с заданной задержкой
- * @param (Function) func
- * @param (number) delay
- */
-module.exports = function(func, delay) {
-  var isThrottled = true;
+module.exports = {
+  /**
+   * Универсальная функция копирования классов с помощью создания
+   * пустого прототипа объекта
+   * @param (Object) ClassOrigin
+   * @return (Object) ClassCopy
+   */
+  inherit: function(ClassOrigin) {
+    var ClassCopy = function() {};
+    ClassCopy.prototype = ClassOrigin.prototype;
 
-  /** Оболочка для оптимизируемой функции */
-  function wrapper() {
-    if (isThrottled) {
-      func();
-      isThrottled = false;
+    return new ClassCopy();
+  },
+
+  /**
+   * Универсальная функция для оптимизации функций с заданной задержкой
+   * @param (Function) func
+   * @param (number) delay
+   * @return {Function} wrapper
+   */
+  throttle: function(func, delay) {
+    var isThrottled = true;
+
+    /** Оболочка для оптимизируемой функции */
+    function wrapper() {
+      if (isThrottled) {
+        func();
+        isThrottled = false;
+      }
+      setTimeout(function() {
+        isThrottled = true;
+      }, delay);
     }
-    setTimeout(function() {
-      isThrottled = true;
-    }, delay);
+    return wrapper;
   }
-  return wrapper;
 };
